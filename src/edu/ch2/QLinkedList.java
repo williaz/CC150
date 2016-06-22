c0d5ddc62fc690a0bfe5282f937cf3e4d7c831b3
Williaz <williaz257@yahoo.com>
2016-06-21 22:30:05 -0400

    2.4-2.5

--- src/edu/ch2/QLinkedList.java
+++ src/edu/ch2/QLinkedList.java
@@ -72,27 +72,170 @@ public class QLinkedList {
 		
 		
 	}
+	
+	//2.4
+	public static<T extends Comparable<T>> void partitionList(LinkedList<T>ll,T x)
+	{
+		ll.remove(ll.indexOf(x));
+		ll.addFirst(x);// put x as head
+		int iX=0;//start from 0
+		
+		//Iterator<T> itr=ll.descendingIterator();
+		T value;
+		
+		for(int i=ll.size()-1;i>=0;)
+		{
+			value=ll.get(i);
+			
+			if(value.compareTo(x)<0)
+			{
+				ll.removeLastOccurrence(value);
+				ll.addFirst(value);
+				iX++;
+			}	
+			else if(value.compareTo(x)==0)
+			{
+				if(ll.lastIndexOf(value)==iX)
+				{
+					//System.out.println("Sure me");
+					break;//break the loop
+				}
+					
+				else
+				{
+					ll.removeLastOccurrence(value);
+					ll.addFirst(value);
+					iX++;
+				}
+			}
+			else
+			{
+				i--;
+			}
+			
+			
+		}
+	
+		
+	}
+	
+	public static LinkedList<Integer> sumDigit(LinkedList<Integer> x,LinkedList<Integer> y)
+	{
+		LinkedList<Integer> sum=new LinkedList<Integer>();
+		//LinkedList<Integer> min=new LinkedList<Integer>();
+	    sum.add(0);
+	    int value;
+	    
+	    Iterator<Integer> iX=x.descendingIterator();
+	    Iterator<Integer> iY=y.descendingIterator();
+	    
+	    while(iX.hasNext()&&iY.hasNext())
+	    {
+	    	value=iX.next()+iY.next()+sum.getFirst();
+	    	
+	    	if(value<=9)
+	    	{
+	    		sum.removeFirst();
+	    		sum.addFirst(value);
+	    		sum.addFirst(0);
+	    	}
+	    	else
+	    	{
+	    		sum.removeFirst();
+	    		sum.addFirst(value-10);
+	    		sum.addFirst(1);
+	    		
+	    	}
+	    }
+	    
+	    if(sum.getFirst().equals(0))
+	    	sum.removeFirst();
+
+		
+		return sum;
+	}
+	
+	public static LinkedList<Integer> sumDigitTail(LinkedList<Integer> x,LinkedList<Integer> y)
+	{
+		LinkedList<Integer> sum=new LinkedList<Integer>();
+		//LinkedList<Integer> min=new LinkedList<Integer>();
+	    sum.addLast(0);;
+	    int value;
+	    
+	    Iterator<Integer> iX=x.iterator();
+	    Iterator<Integer> iY=y.iterator();
+	    
+	    while(iX.hasNext()&&iY.hasNext())
+	    {
+	    	value=iX.next()+iY.next()+sum.getLast();
+	    	
+	    	if(value<=9)
+	    	{
+	    		sum.removeLast();
+	    		sum.addLast(value);
+	    		sum.addLast(0);
+	    	}
+	    	else
+	    	{
+	    		sum.removeLast();
+	    		sum.addLast(value-10);
+	    		sum.addLast(1);
+	    		
+	    	}
+	    }
+	    
+	    if(sum.getLast().equals(0))
+	    	sum.removeLast();
+
+		
+		return sum;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		char[] cc={'F','F', 'O', 'L','L','O','W',' ','U', 'P'};
+		/*// TODO Auto-generated method stub
+		//char[] cc={'F','F', 'O', 'L','L','O','W',' ','U', 'P'};
+		int[] cc={3,4,8,3,1,9,5,2,8,7};
 		
-		LinkedList<Character> l=new LinkedList<Character>();
+		//LinkedList<Character> l=new LinkedList<Character>();
+		LinkedList<Integer> l=new LinkedList<Integer>();
 		for (int i=0;i<cc.length;i++)
 		{
 			
 			l.add(cc[i]);
 		}
 		
-		System.out.println("Before: "+l);
+		System.out.println("Before: "+l+"start from "+l.indexOf('F'));*/
 		
 		//QLinkedList.removeDup(l);
 		//QLinkedList.removeDupNb(l);
 		//System.out.println(l);
 		
 		//System.out.println(QLinkedList.findLastTh(l, 4));
-		QLinkedList.removeMidddle(l);
-		System.out.println(l);
+		//QLinkedList.removeMidddle(l);
+		//QLinkedList.partitionList(l, 5);
+		//System.out.println(l);
+		/*LinkedList<Integer> x=new LinkedList<Integer>();
+		x.add(6);
+		x.add(1);
+		x.add(7);
+		LinkedList<Integer> y=new LinkedList<Integer>();
+		y.add(2);
+		y.add(9);
+		y.add(5);
+		
+		System.out.println(x+" + "+y+" = "+QLinkedList.sumDigit(x, y));*/
+		
+		LinkedList<Integer> x=new LinkedList<Integer>();
+		x.addFirst(6);
+		x.addFirst(1);
+		x.addFirst(7);
+		LinkedList<Integer> y=new LinkedList<Integer>();
+		y.addFirst(2);
+		y.addFirst(9);
+		y.addFirst(5);
+		
+		System.out.println(x+" + "+y+" = "+QLinkedList.sumDigitTail(x, y));
+		
 
 	}
 
